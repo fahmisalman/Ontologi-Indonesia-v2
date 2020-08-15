cd8c30c36d5286c1f0baabcbe9f9ad8d4dbf0c6a
Fahmi Salman Nurfikri <fahmisalman@gmail.com>
2020-08-15 13:52:47 +0700

    Delete db_helper.py

--- db_helper.py
+++
@@ -1,8 +0,0 @@
-import mysql.connector
-
-def connect():
-    db = mysql.connector.connect(host="127.0.0.1",      # your host, usually localhost
-                        user="root",           # your username
-                        passwd="",             # your password
-                        db="db_scbd")          # name of the data base
-    return db
